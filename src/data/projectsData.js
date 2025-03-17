08c3a9e0a0cccabfe928f76046e4daf1df83fbfc
Rahul Krishna Pallath <51940086+Rahulpallath@users.noreply.github.com>
2025-03-16 19:13:00 -0500

    Updated project data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,30 +11,30 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
+        projectName: 'Movie Ticket Booking System',
         projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        tags: ['Javascript','Stripe', 'Java', 'MySQL'],
+        code: 'https://github.com/Rahulpallath/Movie_Management_System',
+        demo: 'https://github.com/Rahulpallath/Movie_Management_System',
         image: one
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Student Coding Platform for University',
+        projectDesc: 'I developed this project as part of my research work, enabling students to solve coding problems, upload notes, and receive automated evaluations, while optimizing performance through database indexing and API caching to improve load times by 25%.',
+        tags: ['.NET', 'Java', 'C#'],
+        code: 'https://github.com/Rahulpallath/OnlineSystemWebApp',
+        demo: 'https://github.com/Rahulpallath/OnlineSystemWebApp',
         image: two
     },
     {
         id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
+        projectName: 'Fairness-Aware News Recommendation System',
+        projectDesc: 'I developed a fairness-aware news recommendation system using Generative Adversarial Networks (GANs) to mitigate bias in personalized news recommendations by reducing bias',
+        tags: ['Python', 'TensorFlow', 'PyTorch', 'GANs', 'Decomposed Adversarial Learning'],
         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
         demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
+        image: seven
     },
     {
         id: 4,
@@ -43,44 +43,8 @@ export const projectsData = [
         tags: ['Flutter', 'Firebase'],
         code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
         demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Uber Lite',
-        projectDesc: 'Uber clone',
-        tags: ['Flutter'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: six
-    },
-    {
-        id: 7,
-        projectName: 'Stock Market App',
-        projectDesc: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
         image: seven
-    },
-    {
-        id: 8,
-        projectName: 'Car Pooling System',
-        projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: eight
-    },
+    }
 ]
 
 
